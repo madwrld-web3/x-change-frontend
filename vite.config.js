39715d5415119291ed48430a1a0ecf34e100031a
madwrld-web3 <madeinruccia@icloud.com>
2025-12-16 16:23:12 +0100

    Fix file structure for Vite build

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [react()],
+    build: {
+        outDir: 'build', // Ensures Vercel finds the output in the right place
+    },
+});
